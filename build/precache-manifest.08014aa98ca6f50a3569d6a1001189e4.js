4c0b9654c95ac4ae1af801cbe64a4bbfb14d7052
Igor Lima <igorlima1740@gmail.com>
2020-05-01 16:34:28 -0300

    deploy do app

---
+++ build/precache-manifest.08014aa98ca6f50a3569d6a1001189e4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d373e0942eeaacc896447dbcff1a309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ea2f3ce2df47fbcbc06",
+    "url": "/static/css/2.6824a4cd.chunk.css"
+  },
+  {
+    "revision": "c9c7c2609e2877d8f9f6",
+    "url": "/static/css/main.adfb997e.chunk.css"
+  },
+  {
+    "revision": "5ea2f3ce2df47fbcbc06",
+    "url": "/static/js/2.adf4895c.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.adf4895c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c7c2609e2877d8f9f6",
+    "url": "/static/js/main.80c8a2bc.chunk.js"
+  },
+  {
+    "revision": "8eca5711379b1f585ed6",
+    "url": "/static/js/runtime-main.fb1964a1.js"
+  },
+  {
+    "revision": "b37296b948436fae54ab77856e5e1c3b",
+    "url": "/static/media/debit-card.b37296b9.png"
+  },
+  {
+    "revision": "4c515d67e6418cc58568f5d29c8c970c",
+    "url": "/static/media/female-professor-of-economy-with-bars-stocks-graphic-on-clipboard.4c515d67.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "c6b87e7b6c3e8cc340889c0f94aacdd5",
+    "url": "/static/media/payment-method.c6b87e7b.png"
+  },
+  {
+    "revision": "a2e5fd40ba0baccfc44435fd1dcc6c14",
+    "url": "/static/media/photo2.a2e5fd40.jpg"
+  },
+  {
+    "revision": "bd42a133ddd51254453109ad41863b1d",
+    "url": "/static/media/photo3.bd42a133.jpg"
+  },
+  {
+    "revision": "445a6a3f389f070a405d16f0c41453fb",
+    "url": "/static/media/success.445a6a3f.png"
+  }
+]);
